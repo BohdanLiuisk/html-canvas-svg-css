15bcd3d929602763f4690e9d84cb264e49362317
TSCRM\b.liusik <b.liusik@creatio.com>
2021-12-14 01:20:40 +0200

    add canvas

---
+++ examples/js/app.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementById("canvas");
+if (canvas.getContext) {
+    const ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = "rgb(200,0,0)";
+    ctx.fillRect (10, 10, 55, 50);
+
+    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+    ctx.fillRect (30, 30, 55, 50);
+}
